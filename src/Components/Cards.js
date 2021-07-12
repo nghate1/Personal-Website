e0acc415f4c8d72493fd7fee5595049923455974
Nik G <nickinnj44@gmail.com>
2021-07-12 14:40:58 -0400

    add new files

---
+++ src/Components/Cards.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {Card} from 'react-bootstrap'
+import '../App.css';
+
+function Cards(props) {
+    return (
+        <Card style={{ width: '12rem' }}>
+                <Card.Body>
+                    <Card.Title>{props.title}</Card.Title>
+                    <Card.Subtitle className="mb-2 text-muted">{props.subtitle}</Card.Subtitle>
+                    <Card.Text>{props.text}</Card.Text>
+                    <Card.Link href={props.addy1}>{props.link1}</Card.Link>
+                    <Card.Link href={props.addy2}>{props.link2}</Card.Link>
+                </Card.Body>
+            </Card>
+    )
+}
+
+export default Cards
